5017cd7b5d7141ca9de730b8c58583955aec7161
Fon Nkwent <fonnkwenti85@gmail.com>
2022-07-06 22:10:22 +0100

    updated tableName

--- backend/users.js
+++ backend/users.js
@@ -1,17 +1,22 @@
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-1" });
+//const { v4: uuid } = require("uuid");
+
+const { v4 } = require("uuid");
 
 // create the DynamoDB Document Client
-const ddd = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-const tableName = process.env.DYNAMODB_TABLE;
+const tableName = process.env.DYNAMODB_TABLE_NAME;
+console.log(tableName);
 
 module.exports.create = async (event) => {
   const body = JSON.parse(event.body);
-  if (!body.includes(firstName)) {
-    console.log("Validation Error! Please create First Name");
-  }
-  const uniqueId = uuid().toString();
+  console.log(body);
+  //   if (!body.includes(firstName)) {
+  //     console.log("Validation Error! Please create First Name");
+  //   }
+  const uniqueId = v4().toString();
 
   const timeStamp = new Date().toISOString();
 
@@ -22,8 +27,8 @@ module.exports.create = async (event) => {
       //   sk: "PROFILE#" + customer_id,
       //   profile_data: profile_data,
 
-      PK: "USER#" + uniqueId,
-      SK: "METADATA#" + uniqueId,
+      PK: `USER#${uniqueId}`,
+      SK: `METADATA#${uniqueId}`,
       userId: uniqueId,
       firstName: body.firstName,
       lastName: body.lastName,
@@ -34,24 +39,18 @@ module.exports.create = async (event) => {
   };
   try {
     let createUser;
-    createUser = await ddd.put(putParams).promise();
+    createUser = await ddb.put(putParams).promise();
 
     return {
       //statuscode: StatusCode || 200,
       statusCode: 200,
       body: JSON.stringify({
         message: "Successfully create user",
-        createUser,
       }), // return us an object of the Items that have been written to dynamoDB
     };
   } catch (error) {
     console.log(error);
-    Response.statusCode = 500;
-    Response.body = JSON.stringify({
-      message: "Failed to create user",
-      errorMessage: error.message,
-      errorStack: error.stack,
-    });
+
     throw new Error(error);
   }
 };
